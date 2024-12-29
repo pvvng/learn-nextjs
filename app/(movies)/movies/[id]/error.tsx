f49e444b86f2e93252288c086968bda7c11de7e0
맥동우 <t6u80o@naver.com>
2024-12-29 16:49:45 +0900

    에러 핸들링을 error.tsx로 해보자

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function MovieDetailError() {
+  return <h1>something broke...</h1>;
+}
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,7 +2,10 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
   // video data 불러오는데만 시간 지연 발생상황 가정
-  // await new Promise(resolve => setTimeout(resolve, 5000));
+  // await new Promise(resolve => setTimeout(resolve, 3000));
+
+  // 에러 상황 가정
+  // throw new Error('something broke..');
 
   const response = await fetch(`${API_URL}/${id}/videos`, { cache: "force-cache" });
 
